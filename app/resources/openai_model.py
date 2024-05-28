51568cc2fe3057500f9ca942e7a0d6a1b5358bb7
Vitor Pereira <vitorpereira@MacBook-Air-de-Vitor.local>
2024-05-28 13:29:12 -0300

    adjusts openai module

--- app/resources/openai_model.py
+++ app/resources/openai_model.py
@@ -1,13 +1,14 @@
 import os 
 import openai
 from dotenv import load_dotenv, find_dotenv
-from flask import jsonify
+import logging
 
-from head_prompt import HEAD_PROMPT
+from .head_prompt import HEAD_PROMPT
 
 load_dotenv(find_dotenv())
 
 openai = openai.OpenAI(api_key= os.environ.get('OPENAI_API_KEY'))
+logger = logging.getLogger('open-ai-module-log')
 
 class OpenAiModel:
     def __init__(self):
@@ -17,21 +18,23 @@ class OpenAiModel:
         try:
             response = openai.chat.completions.create(
                 model=self.model,
-                messages=self.conversation_string(user_message),
+                messages=self.conversation_string(user_message)
             )
 
-            return response["choices"][0]["message"]["content"]
+            assistant_answer = response.choices[0].message.content
+            logger.info(f"ASSISTANT_ANSWER: {assistant_answer}")
+            return assistant_answer
         except Exception as e:
             raise e
 
     def conversation_string(self, user_message):
-        [
-            {
-                'role': 'system',
-                "content": f"{HEAD_PROMPT}"
-            }, 
-            {
-                'role': 'user',
-                'content': f"{user_message}"
-            }
-        ]
+        return [
+                    {
+                        'role': 'system',
+                        "content": f'{HEAD_PROMPT}'
+                    }, 
+                    {
+                        'role': 'user',
+                        'content': f'{user_message}'
+                    }
+                ]
