8520a4d2e0e68cec53aee56cf5771f463b071dbf
Vitor Pereira <vitorpereira@MacBook-Air-de-Vitor.local>
2024-05-28 13:27:25 -0300

    adds logging

--- app/main.py
+++ app/main.py
@@ -4,10 +4,23 @@ from dotenv import load_dotenv
 import os
 from db import db
 from routes import bp
+import logging
 
-app = Flask(__name__)
+def app():
 
-app.register_blueprint(bp)
+    app = Flask(__name__)
+    logger = logging.getLogger(__name__)
+
+    logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            )
+    logger.info('Application started')
+
+    app.register_blueprint(bp)
+
+    return app.run(debug=True)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app()
