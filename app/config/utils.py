2cf6334fd2559f177ef8d6da4539f4605c31951b
Vitor Pereira <vitorpereira@MacBook-Air-de-Vitor.local>
2024-08-20 17:46:06 -0300

    adds bots config

--- app/config/utils.py
+++ app/config/utils.py
@@ -1,11 +1,12 @@
 from flask import request, jsonify
 import os
 from dotenv import load_dotenv, find_dotenv
+import yaml
 
 load_dotenv(find_dotenv())
 
 
-def require_api_key(f):
+def require_api_key(func):
     def decorator(*args, **kwargs):
         api_key = request.headers.get("x-api-key")
         if not api_key or api_key != os.environ.get(
@@ -13,7 +14,15 @@ def require_api_key(f):
         ):
             response = jsonify({"message": "Invalid or missing API key"}), 401
             return response
-        return f(*args, **kwargs)
+        return func(*args, **kwargs)
 
-    decorator.__name__ = f.__name__
+    decorator.__name__ = func.__name__
     return decorator
+
+def retrieve_prompt(bot_name):
+    with open("bots_config.yml") as config:
+        try:
+            content = yaml.safe_load(config)
+            return content['bots'][bot_name]['head_prompt'][0]
+        except yaml.YAMLError as exc:
+            raise Exception(f"An exception occurred while loading config files: {exc}")
