93c7b6477e8249feeec7aa228284c96cefdfa567
vitorpereira <paulovgp@id.uff.br>
2024-08-21 17:15:54 -0300

    adds Conversation class with register_conversation method

--- app/models/conversation.py
+++ app/models/conversation.py
@@ -1,4 +1,6 @@
-from db import db
+from models.db import db
+import logging
+
 
 class ConversationModel(db.Model):
     __tablename__ = "conversations"
@@ -7,3 +9,39 @@ class ConversationModel(db.Model):
     bot_name = db.Column(db.String)
     user_message = db.Column(db.String)
     agent_answer = db.Column(db.String)
+
+
+class Conversation:
+    logger = logging.getLogger("[ConversationModel]")
+
+    @classmethod
+    def register_conversation(
+            cls,
+            bot_name: str,
+            user_message: str,
+            agent_answer: str
+    ) -> str:
+        cls.logger.info(
+            "[RegisterConversation] Starting to register conversation"
+        )
+
+        try:
+            conversation = ConversationModel(
+                bot_name=bot_name,
+                user_message=user_message,
+                agent_answer=agent_answer
+            )
+
+            db.session.add(conversation)
+            db.session.commit()
+
+            cls.logger.info(
+                f"[RegisterConversation] Finishing to register conversation: {conversation}"
+            )
+            cls.logger.info(
+                "[RegisterConversation] Finished registering conversation"
+            )
+
+            return "Conversation registered"
+        except Exception as exc:
+            raise exc
