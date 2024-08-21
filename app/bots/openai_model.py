71025447ecedfe38e7e1ad0644cabef7ef3e0f30
vitorpereira <paulovgp@id.uff.br>
2024-08-21 17:18:08 -0300

    adds Conversation registering step to OpenAiModel class

--- app/bots/openai_model.py
+++ app/bots/openai_model.py
@@ -2,7 +2,8 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv
 import logging
-from app.bots.context_retrieval.pinecone_search import PineconeSearch
+from bots.context_retrieval.pinecone_search import PineconeSearch
+from models.conversation import Conversation
 
 load_dotenv(find_dotenv())
 
@@ -10,28 +11,35 @@ openai = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 logger = logging.getLogger("OpenAiModel")
 
 
-class OpenAiModel:
-    def __init__(self):
+class OpenAiModel():
+    def __init__(self, bot_name):
         self.model = "gpt-3.5-turbo"
+        self.bot_name = bot_name
         self.pinecone_search = PineconeSearch()
-
 
     def generate_conversation(self, user_message, head_prompt=None):
         system_prompt = head_prompt if head_prompt else ''
 
         try:
             assistant_answer = openai.chat.completions.create(
-                model = self.model,
-                messages = self.conversation_string(
-                system_prompt, 
-                user_message
+                model=self.model,
+                messages=self.conversation_string(
+                    system_prompt,
+                    user_message
                 )
             ).choices[0].message.content
             logger.info(f"ASSISTANT_ANSWER: {assistant_answer}")
+
+            logger.info(f"Starting to commit conversation")
+
+            Conversation.register_conversation(
+                bot_name=self.bot_name,
+                agent_answer=assistant_answer,
+                user_message=user_message
+            )
             return assistant_answer
         except Exception as e:
             raise e
-
 
     @staticmethod
     def conversation_string(system_prompt, user_message):
