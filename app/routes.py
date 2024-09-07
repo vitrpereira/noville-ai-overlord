197ccd21c166eb9cba570207987a1dc6323376bf
vitorpereira <paulovgp@id.uff.br>
2024-09-07 18:06:40 -0300

    adds new routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
 from app.bots.openai_model import OpenAiModel
 from app.bots.leo.leo import Leo
+from app.bots.veronica.veronica import Veronica
 from app.config.utils import require_api_key
+from app.connections.external_apis.twilio_api import TwilioApi
 import logging
 
 logger = logging.getLogger("app_log")
@@ -18,6 +20,30 @@ def generate_conversation():
         return jsonify({"message": "You must input a message!"}), 400
     conversation = OpenAiModel("EndpointClient").generate_conversation(param)
     return jsonify({"message": f"{conversation}"})
+
+
+@bp.route("/overlord/veronica", methods=["POST"])
+@require_api_key
+def temp_text_to_audio():
+    data = request.json
+    param = data["message"]
+
+    if not param or "message" not in data:
+        return jsonify({"message": "You must input a message!"}), 400
+    conversation = Veronica().call(param)
+    return jsonify({"message": conversation})
+
+
+@bp.route("/overlord/twilio_messager", methods=["POST"])
+@require_api_key
+def twilio_messager():
+    data = request.json
+    param = data["message"]
+
+    if not param or "message" not in data:
+        return jsonify({"message": "You must input a message!"}), 400
+    conversation = TwilioApi.post(media_file=param)
+    return jsonify({"message": conversation})
 
 
 @bp.route("/overlord/test_leo", methods=["POST"])
